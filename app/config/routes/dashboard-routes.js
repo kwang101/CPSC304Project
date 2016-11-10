e9268e75a01a0e323100e5a2905b90e48b38353d
Geordie Parappilly <geordie.p@hotmail.com>
2016-11-10 13:18:58 -0800

    trying to get it to display on the website. i can console log the data, but i havent firgured out how to parse it yet.

--- app/config/routes/dashboard-routes.js
+++ app/config/routes/dashboard-routes.js
@@ -1,17 +1,44 @@
-module.exports = function(app) {
+module.exports = function (app) {
   /**
    * Display Home Page
   **/
-  
 
+  app.get('/dashboard', function (req, res) {
+    const connection = require('../connection.js');
+    var programType;
+    var term;
+    var price;
+    var name;
+    var programId;
+    connection.query('SELECT * FROM cpsc304_test.program', function (err, rows, fields) {
+      if (err)
+        console.log('Error while performing Query.');
+      else
+        console.log(rows);
+        //console.log(rows.length);
+        //for (var i = 0; i < rows.length; i++) {
+        //  programType.push(rows[i]['programType']);
+        //}
+        //console.log(programType);
 
-app.get('/dashboard', function(req, res) {
-    res.render('dashboard', {
-      title: 'Dashboard',
-      message: 'Dashboard',
-      userName: (req.user) ? req.user.username : undefined,
-      flashMessage: req.flash('flashMessage')
+      res.render('dashboard', {
+        title: 'Dashboard',
+        message: 'Dashboard',
+        userName: (req.user) ? req.user.username : undefined,
+        flashMessage: req.flash('flashMessage'),
+        // programType: programType,
+        // term: term,
+        // price: price,
+        // name: name,
+        // programId: programId
+
+      });
     });
+
   });
+
+  //SELECT * FROM cpsc304_test.program;
+
+
 
 }
