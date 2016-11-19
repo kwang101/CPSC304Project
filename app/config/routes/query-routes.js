7f3a06dceee1fe79a044d36f2842ec8f5d13ff4f
Geordie Parappilly <geordie.p@hotmail.com>
2016-11-19 01:51:50 -0800

    later than time query done.

--- app/config/routes/query-routes.js
+++ app/config/routes/query-routes.js
@@ -1,16 +1,16 @@
-module.exports = function(app) {
+module.exports = function (app) {
 
     // var requiresLogin = require('./auth-routes').requiresLogin;
     // var hasAuthorization = require('./auth-routes').hasAuthorization;
 
     app.get('/lowerthan/*',
         // requiresLogin, hasAuthorization({ isUBC: 0, isAdmin: 1, isInstructor: 0}), 
-        function(req, res) {
+        function (req, res) {
             const connection = require('../connection.js');
             var identification = req.params['0'];
             connection.query('SELECT * FROM program WHERE price <= ?',
                 [identification],
-                function(err, programs, fields) {
+                function (err, programs, fields) {
                     if (err)
                         console.log('Error while performing Query.');
                     else
@@ -27,23 +27,55 @@ module.exports = function(app) {
         })
 
     app.post('/lowerthan/*',
-        function(req, res) {
+        function (req, res) {
             var input = req.body.number;
             console.log(input);
             res.redirect('../lowerthan/' + input);
         }
     );
 
+    //laterthan
+    app.get('/laterthan/*',
+        // requiresLogin, hasAuthorization({ isUBC: 0, isAdmin: 1, isInstructor: 0}), 
+        function (req, res) {
+            const connection = require('../connection.js');
+            var identification = req.params['0'];
+            connection.query('SELECT programType, term, name, startTime, endTime, dayOfWeek, program.programId FROM program JOIN occurs ON occurs.programId = program.programId AND occurs.startTime >= ?;',
+                [identification],
+                function (err, programs, fields) {
+                    if (err)
+                        console.log('Error while performing Query.');
+                    else
+                        //     console.log(identification);
+                        // console.log(programs);
+                        res.render('laterthan', {
+                            title: identification,
+                            message: identification,
+                            userName: (req.user) ? req.user.username : undefined,
+                            flashMessage: req.flash('flashMessage'),
+                            programs: programs
+                        });
+                });
+        })
+
+    app.post('/laterthan/*',
+        function (req, res) {
+            var input = req.body.number;
+            console.log(input);
+            res.redirect('../laterthan/' + input);
+        }
+    );
+
     //most expensive and least expensive aggregation
     app.get('/expensive/*',
         // requiresLogin, hasAuthorization({ isUBC: 0, isAdmin: 1, isInstructor: 0}), 
-        function(req, res) {
+        function (req, res) {
             const connection = require('../connection.js');
             var identification = req.params['0'];
             console.log(identification);
             if (identification == "most") {
                 connection.query('SELECT * FROM program WHERE price = (SELECT MAX(price) FROM program);',
-                    function(err, programs, fields) {
+                    function (err, programs, fields) {
                         if (err)
                             console.log('Error while performing Query.');
                         else
@@ -60,7 +92,7 @@ module.exports = function(app) {
             }
             else
                 connection.query('SELECT * FROM program WHERE price = (SELECT MIN(price) FROM program);',
-                    function(err, programs, fields) {
+                    function (err, programs, fields) {
                         if (err)
                             console.log('Error while performing Query.');
                         else
@@ -79,13 +111,13 @@ module.exports = function(app) {
 
     app.get('/avg/*',
         // requiresLogin, hasAuthorization({ isUBC: 0, isAdmin: 1, isInstructor: 0}), 
-        function(req, res) {
+        function (req, res) {
             const connection = require('../connection.js');
             var identification = req.params['0'];
             console.log(identification);
             if (identification == "price") {
                 connection.query('SELECT programType, AVG(price) AS average FROM program GROUP BY programType',
-                    function(err, rows, fields) {
+                    function (err, rows, fields) {
                         if (err)
                             console.log('Error while performing Query.');
                         else
@@ -102,13 +134,13 @@ module.exports = function(app) {
             }
             else
                 connection.query('SELECT programType, AVG(price) AS average FROM program GROUP BY programType',
-                    function(err, rows, fields) {
+                    function (err, rows, fields) {
                         if (err)
                             console.log('Error while performing Query.');
                         else
                             //     console.log(identification);
                             console.log(rows);
-                            rows['AVG(price)'];
+                        rows['AVG(price)'];
                         res.render('avg', {
                             title: identification,
                             message: 'Most Expensive Programs',
@@ -119,14 +151,14 @@ module.exports = function(app) {
                     });
         })
 
-        app.get('/division',
+    app.get('/division',
         // requiresLogin, hasAuthorization({ isUBC: 0, isAdmin: 1, isInstructor: 0}), 
-        function(req, res) {
+        function (req, res) {
             const connection = require('../connection.js');
             var identification = req.params['0'];
             connection.query('SELECT s.name FROM cpsc304_test.user s WHERE NOT EXISTS (SELECT programId FROM cpsc304_test.classes p WHERE NOT EXISTS (SELECT s.userId FROM cpsc304_test.registers r WHERE s.userId=r.userId AND p.programId = r.programId));',
                 [identification],
-                function(err, users, fields) {
+                function (err, users, fields) {
                     if (err)
                         console.log('Error while performing Query.');
                     else
--- app/seed-db.js
+++ app/seed-db.js
@@ -24,6 +24,7 @@ module.exports = function () {
             connection.query(
                 `
                 INSERT INTO Program VALUES ('class', 2, 20.00, 'Yoga', 000132);
+                INSERT INTO Program VALUES ('class', 2, 50.00, 'Twerking', 000134);
                 INSERT INTO Program VALUES ('class', 1, 15.00, 'Pilates', 000135);
                 INSERT INTO Program VALUES ('class', 2, 20.00, 'T-Bagging', 000137);
                 INSERT INTO Program VALUES ('class', 1, 15.00, 'Meme-ing', 000133);
@@ -43,6 +44,7 @@ module.exports = function () {
                 `
                 INSERT INTO Occurs VALUES ('19:00:00', '20:00:00', 1, 000132);
                 INSERT INTO Occurs VALUES ('20:00:00', '21:00:00', 1, 000133);
+                INSERT INTO Occurs VALUES ('23:00:00', '24:00:00', 5, 000134);
                 INSERT INTO Occurs VALUES ('19:00:00', '20:00:00', 2, 000135);
                 INSERT INTO Occurs VALUES ('20:00:00', '21:00:00', 2, 000136);
                 INSERT INTO Occurs VALUES ('19:00:00', '20:00:00', 3, 000137);
@@ -94,6 +96,7 @@ module.exports = function () {
                 `
                 INSERT INTO IsLocated VALUES ('Gym 1', '1 West Mall', 000132);
                 INSERT INTO IsLocated VALUES ('Hugh Dempster Pavillion', '6245 Agronomy Road', 000133);
+                INSERT INTO IsLocated VALUES ('Gym 3', '1 West Mall', 000134);
                 INSERT INTO IsLocated VALUES ('Birdcoop', '1 West Mall', 000135);
                 INSERT INTO IsLocated VALUES ('Birdcoop', '1 West Mall', 000136);
                 INSERT INTO IsLocated VALUES ('Gym 2', '1 West Mall', 000137);
@@ -115,6 +118,7 @@ module.exports = function () {
                 INSERT INTO TeachesClass VALUES (000137, 12345678);
                 INSERT INTO TeachesClass VALUES (000133, 09289345);
                 INSERT INTO TeachesClass VALUES (000136, 18237481);
+                INSERT INTO TeachesClass VALUES (000134, 12345678);
                 `,
                 function (err, result) {
                     if (err && err.code !== 'ER_DUP_KEY' && err.code !== 'ER_DUP_ENTRY') callback(err);
