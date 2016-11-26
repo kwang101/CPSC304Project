e54972b1ecb451dd83feec19997023df3f56bfd8
Geordie Parappilly <geordie.p@hotmail.com>
2016-11-25 22:55:07 -0800

    error page fix. userid on home page

--- app/config/routes/default-routes.js
+++ app/config/routes/default-routes.js
@@ -3,13 +3,27 @@ module.exports = function (app) {
    * Display Home Page
   **/
   app.get('/', function (req, res) {
+    console.log('boom');
     console.log(req.user);
+    console.log('ba');
+    if (req.user != null) {console.log(req.user.userId);
     res.render('index', {
       title: 'Your page title',
       message: 'Your header message',
       userName: (req.user) ? req.user.username : undefined,
-      flashMessage: req.flash('flashMessage')
+      flashMessage: req.flash('flashMessage'),
+      userId: req.user.userId
     });
+    }
+    else {
+      res.render('index', {
+      title: 'Your page title',
+      message: 'Your header message',
+      userName: (req.user) ? req.user.username : undefined,
+      flashMessage: req.flash('flashMessage'),
+      userId: ""
+    });
+    }
   });
 
   // app.post('/',
@@ -25,12 +39,12 @@ module.exports = function (app) {
     function (req, res) {
       //this is for user
       const connection = require('../connection.js');
-      console.log('user');
-      console.log(req.body.user);
-      console.log('admin');
-      console.log(req.body.admin);
+      //console.log('user');
+      //console.log(req.body.user);
+      //console.log('admin');
+      //console.log(req.body.admin);
       var input = req.body.number;
-      console.log(input);
+      //console.log(input);
       if (req.body.user != undefined) {
         res.redirect('../users/' + input);
         //this is for admin
@@ -38,7 +52,7 @@ module.exports = function (app) {
         connection.query('SELECT * FROM user WHERE userId IN (SELECT userId FROM user WHERE userId = ? AND isAdmin = 1)',
           [input],
           function (err, adminexists, fields) {
-            console.log(adminexists);
+            //console.log(adminexists);
             if (adminexists.length == 0)
               res.render('error', {
                 message: 'Nice try buddy. You ain\'t no Admin!'
@@ -50,7 +64,7 @@ module.exports = function (app) {
         connection.query('SELECT * FROM user WHERE userId IN (SELECT userId FROM user WHERE userId = ? AND isInstructor = 1)',
           [input],
           function (err, instructorexists, fields) {
-            console.log(instructorexists);
+            //console.log(instructorexists);
             if (instructorexists.length == 0)
               res.render('error', {
                 message: 'Nice try buddy. You ain\'t no Instructor!'
@@ -59,5 +73,5 @@ module.exports = function (app) {
               res.redirect('../instructor/' + input);
           });
       }
-    })
+    });
 }
