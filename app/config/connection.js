5b7a5d9039b86b11d79df401947c190b6814f542
David Baldwynn <shorerydr10@live.com>
2016-11-28 19:24:33 -0800

    added default config

--- app/config/connection.js
+++ app/config/connection.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql');
 const pool = mysql.createPool({
-  host     : process.env.DB_HOST || 'sulnwdk5uwjw1r2k.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-  user     : process.env.DB_USER || 'g89pjlxdyo776ioi',
-  password : process.env.DB_PASSWORD || 'ok8pai8mvwqhn5ck',
-  database : process.env.DB_NAME || 'okep5tr7f8lcolsi',
+  host     : process.env.DB_HOST || 'localhost', //'sulnwdk5uwjw1r2k.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+  user     : process.env.DB_USER || 'root', //'g89pjlxdyo776ioi',
+  password : process.env.DB_PASSWORD || '', //'ok8pai8mvwqhn5ck',
+  database : process.env.DB_NAME || 'cpsc304_test', //'okep5tr7f8lcolsi',
   port: process.env.DB_PORT || '3306',
   debug: true,
   multipleStatements: true
